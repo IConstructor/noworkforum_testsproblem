dd39b1d62cbca6f090bcad8981bb99a5d0f2a966
Denys Aleksieiev <Denys_Aleksieiev@epam.com>
2016-01-02 13:19:52 +0200

    7

--- NoWorkForum/App_Start/WebApiConfig.cs
+++ NoWorkForum/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace NoWorkForum.WebApi
 {
@@ -9,7 +10,9 @@ namespace NoWorkForum.WebApi
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+
 
             // Web API routes
             config.MapHttpAttributeRoutes();
